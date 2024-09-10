5f107c0b5ab3732e43ef9ba9e8bb1335114818ee
arh <a.hafizi9@outlook.com>
2024-09-10 18:43:27 +0330

    converting from HTTP to NATS

--- apps/workflows-service/src/main.ts
+++ apps/workflows-service/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
+
+    app.connectMicroservice<MicroserviceOptions>(
+      {
+        transport: Transport.NATS,
+        options: {
+          servers: process.env.NATS_URL,
+        },
+      },
+      { inheritAppConfig: true },
+    );
+    await app.startAllMicroservices();
+  
     await app.listen(3001);
 }
 bootstrap();
--- apps/workflows-service/src/workflows/workflows.controller.ts
+++ apps/workflows-service/src/workflows/workflows.controller.ts
@@ -1,21 +1,21 @@
-import {
-    Controller,
-    Get,
-    Post,
-    Body,
-    Patch,
-    Param,
-    Delete,
-} from '@nestjs/common';
-import { WorkflowsService } from './workflows.service';
 import { CreateWorkflowDto, UpdateWorkflowDto } from '@app/workflows';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch
+} from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { WorkflowsService } from './workflows.service';
 
 @Controller('workflows')
 export class WorkflowsController {
     constructor(private readonly workflowsService: WorkflowsService) {}
 
-    @Post()
-    create(@Body() dto: CreateWorkflowDto) {
+    @MessagePattern('workflows.create') 
+    create(@Payload() dto: CreateWorkflowDto) {
         return this.workflowsService.create(dto);
     }
 
